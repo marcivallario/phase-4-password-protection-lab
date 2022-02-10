05b3b4f6e80258b152f39462eb08c2202e20a5aa
marcivallario <68357009+marcivallario@users.noreply.github.com>
2022-02-10 13:39:00 -0500

    commit tree cbf799c96cb4bd7b4c81121380f485186968363a

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    rescue_from ActiveRecord::RecordNotFound, with: :render_notfound
+    before_action :authorize, only: [:show]
+    
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
+
+    def show
+        user = User.find_by(id: session[:user_id])
+        render json: user, status: :ok
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+    end
+
+    def render_notfound
+        render json: { error: "Not authorized" }, status: :unauthorized
+    end
+
+    def authorize
+        render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+    end
+
+end
